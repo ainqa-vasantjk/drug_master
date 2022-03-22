643652abbf21cbc51036c2c23beaf359bc9f58ab
vasantjk <vasant.jayakumar@ainqa.com>
2022-03-22 14:45:35 +0530

    deleted

--- react_drugmaster_v_2022_3_21_3_30PM_15/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author AUTHOR_NAME
- * @email AUTHOR_EMAIL
- * @create date
- * @modify date
- * @desc Exporting all the assets from /src/assets
- */
--- react_drugmaster_v_2022_3_21_3_30PM_15/src/redux/index.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer as reducer } from "drugmaster_v_dbinder";
-
-export const store = configureStore({
-  reducer,
-});
